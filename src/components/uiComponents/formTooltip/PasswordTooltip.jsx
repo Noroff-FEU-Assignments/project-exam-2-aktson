c8fa3797697130dbbad0186e8bb557474d7fffce
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-22 21:00:48 +0100

    tooltip color changed

--- src/components/uiComponents/formTooltip/PasswordTooltip.jsx
+++ src/components/uiComponents/formTooltip/PasswordTooltip.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function PasswordTooltip({ message }) {
 	return (
 		<p
-			className="absolute -top-2 left-16 w-max text-xs bg-grey  text-white  pb-3 px-1 shadow-2xl opacity-80"
+			className="absolute -top-2 left-16 w-max text-xs bg-grey  text-white  pb-3 px-1 shadow-xl"
 			style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 63%, 74% 63%, 59% 100%, 64% 63%, 0 62%)" }}>
 			{message}
 		</p>
--- src/components/uiComponents/modals/editProfile/editBanner/EditBanner.jsx
+++ src/components/uiComponents/modals/editProfile/editBanner/EditBanner.jsx
@@ -1,139 +1,128 @@
-import React from 'react';
-import { PROFILES_URL, CLOUD_KEY, CLOUD_NAME } from '../../../../constants/api';
+import React from "react";
+import { PROFILES_URL, CLOUD_KEY, CLOUD_NAME } from "../../../../constants/api";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, IconButton, Input } from "@material-tailwind/react";
-import { MdClear, MdImage, MdCached, MdModeEdit } from "react-icons/md"
-import ModalContext from '../../../../context/ModalContext';
-import AuthContext from '../../../../context/AuthContext';
-import Form from '../../Form';
-import ErrorSpan from '../../../ErrorSpan';
-import useAxios from '../../../../hooks/useAxios';
-import { toast } from 'react-toastify';
-import AdminContext from '../../../../context/AdminContext';
-import Spinner from '../../../loader/Spinner';
-import axios from "axios"
-import { imageValidation } from '../../../../yupSchema/imageValidation';
-import ModalEditBanner from './ModalEditBanner';
-
-
-
+import { MdClear, MdImage, MdModeEdit } from "react-icons/md";
+import ModalContext from "../../../../context/ModalContext";
+import AuthContext from "../../../../context/AuthContext";
+import Form from "../../Form";
+import ErrorSpan from "../../../ErrorSpan";
+import useAxios from "../../../../hooks/useAxios";
+import { toast } from "react-toastify";
+import AdminContext from "../../../../context/AdminContext";
+import Spinner from "../../../loader/Spinner";
+import axios from "axios";
+import { imageValidation } from "../../../../yupSchema/imageValidation";
+import ModalEditBanner from "./ModalEditBanner";
 
 function EditBanner() {
+	const http = useAxios();
 
-    const http = useAxios();
+	const { openEditBannerModal, closeEditBannerModal } = React.useContext(ModalContext);
+	const { auth, setAuth } = React.useContext(AuthContext);
+	const { setUpdateAdminUi } = React.useContext(AdminContext);
+	const [error, setError] = React.useState(null);
 
-    const { openEditBannerModal, closeEditBannerModal } = React.useContext(ModalContext);
-    const { auth, setAuth } = React.useContext(AuthContext);
-    const { setUpdateAdminUi } = React.useContext(AdminContext)
-    const [error, setError] = React.useState(null)
+	const [isSubmitting, setIsSubmitting] = React.useState(false);
 
-    const [isSubmitting, setIsSubmitting] = React.useState(false);
+	// react hook form and yup schema
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm({ resolver: yupResolver(imageValidation) });
 
-    // react hook form and yup schema
-    const { handleSubmit, register, formState: { errors } } = useForm({ resolver: yupResolver(imageValidation) });
+	// uploads image to cloudinary and passes url to put request for noroff url
+	const uploadImage = async (data) => {
+		const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
 
-    // uploads image to cloudinary and passes url to put request for noroff url
-    const uploadImage = async (data) => {
+		setIsSubmitting(true);
 
-        const url = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+		try {
+			const formdata = new FormData();
 
-        setIsSubmitting(true)
+			formdata.append("file", data.image[0]);
+			formdata.append("upload_preset", CLOUD_KEY);
+			formdata.append("folder", "social_app");
 
-        try {
-            const formdata = new FormData();
+			const response = await axios.post(url, formdata);
 
-            formdata.append("file", data.image[0])
-            formdata.append("upload_preset", CLOUD_KEY)
-            formdata.append("folder", "social_app")
+			if (response.data.url) {
+				handleBannerSubmit(response.data.url);
+			}
+		} catch (error) {
+			console.log(error);
+			setError("Could not upload image");
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
 
-            const response = await axios.post(url, formdata)
+	// gets image url from cloudinary and makes request to noroff api
+	const handleBannerSubmit = async (imageUrl) => {
+		const url = `${PROFILES_URL}/${auth?.name}/media`;
 
-            if (response.data.url) {
-                handleBannerSubmit(response.data.url)
-            }
+		const authCopy = { ...auth, banner: imageUrl };
 
-        } catch (error) {
-            console.log(error)
-            setError("Could not upload image")
+		setIsSubmitting(true);
 
-        } finally {
-            setIsSubmitting(false)
-        }
-    }
+		try {
+			const response = await http.put(url, { banner: imageUrl });
 
-    // gets image url from cloudinary and makes request to noroff api 
-    const handleBannerSubmit = async (imageUrl) => {
+			if (response) {
+				closeEditBannerModal();
+				setUpdateAdminUi(response.data);
+				setAuth(authCopy);
+				toast.success("Banner updated!");
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error("Could not update profile");
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
 
-        const url = `${PROFILES_URL}/${auth?.name}/media`;
-
-        const authCopy = { ...auth, banner: imageUrl }
-
-        setIsSubmitting(true)
-
-        try {
-            const response = await http.put(url, { "banner": imageUrl });
-
-            if (response) {
-                closeEditBannerModal();
-                setUpdateAdminUi(response.data)
-                setAuth(authCopy)
-                toast.success("Banner updated!")
-            }
-
-        } catch (error) {
-            console.log(error)
-            toast.error("Could not update profile")
-
-        } finally {
-            setIsSubmitting(false)
-        }
-    }
-
-    return (
-        <>
-            <Button
-                color="cyan"
-                variant='text'
-                size='sm'
-                className='flex gap-2 items-center'
-                onClick={openEditBannerModal}>
-                <MdModeEdit size={18} />
-                Banner
-            </Button>
-            <ModalEditBanner >
-                <Form >
-                    <div className='flex justify-between items-center mb-8' >
-                        <h2>Update Banner</h2>
-                        <IconButton variant="text" className='text-grey' onClick={closeEditBannerModal}>
-                            <MdClear size={24} />
-                        </IconButton>
-                    </div>
-                    <fieldset className='flex flex-col gap-6 p-2' disabled={isSubmitting} >
-                        <div>
-                            <Input
-                                {...register("image")}
-                                label="Banner"
-                                size="lg"
-                                variant="standard"
-                                color="cyan"
-                                type="file"
-                                icon={<MdImage size={20} />} />
-                            {errors.image && <ErrorSpan message={errors.image.message} />}
-                            {error && <ErrorSpan message={error} />}
-                        </div>
-                        <div className='flex justify-end mt-4'>
-                            <Button onClick={handleSubmit(uploadImage)} color="cyan" className="flex gap-2 items-center btn">
-                                <Spinner isSubmitting={isSubmitting} />
-                                {isSubmitting ? "Updating" : "Update"}
-                            </Button>
-                        </div>
-                    </fieldset>
-                </Form>
-            </ModalEditBanner>
-        </>
-    )
+	return (
+		<>
+			<Button color="cyan" variant="text" size="sm" className="flex gap-2 items-center" onClick={openEditBannerModal}>
+				<MdModeEdit size={18} />
+				Banner
+			</Button>
+			<ModalEditBanner>
+				<Form>
+					<div className="flex justify-between items-center mb-8">
+						<h2>Update Banner</h2>
+						<IconButton variant="text" className="text-grey" onClick={closeEditBannerModal}>
+							<MdClear size={24} />
+						</IconButton>
+					</div>
+					<fieldset className="flex flex-col gap-6 p-2" disabled={isSubmitting}>
+						<div>
+							<Input
+								{...register("image")}
+								label="Banner"
+								size="lg"
+								variant="standard"
+								color="cyan"
+								type="file"
+								icon={<MdImage size={20} />}
+							/>
+							{errors.image && <ErrorSpan message={errors.image.message} />}
+							{error && <ErrorSpan message={error} />}
+						</div>
+						<div className="flex justify-end mt-4">
+							<Button onClick={handleSubmit(uploadImage)} color="cyan" className="flex gap-2 items-center btn">
+								<Spinner isSubmitting={isSubmitting} />
+								{isSubmitting ? "Updating" : "Update"}
+							</Button>
+						</div>
+					</fieldset>
+				</Form>
+			</ModalEditBanner>
+		</>
+	);
 }
 
-export default EditBanner
-
+export default EditBanner;
