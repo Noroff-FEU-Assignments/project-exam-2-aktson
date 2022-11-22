d1c19e789ee3d238285dae0237ec0131b4a3c31d
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-22 21:07:30 +0100

    tooltip further styled

--- src/components/uiComponents/formTooltip/PasswordTooltip.jsx
+++ src/components/uiComponents/formTooltip/PasswordTooltip.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { MdInfoOutline } from "react-icons/md";
 
 function PasswordTooltip({ message }) {
 	return (
 		<p
-			className="absolute -top-2 left-16 w-max text-xs bg-grey  text-white  pb-3 px-1 shadow-xl"
+			className="absolute -top-2 left-16 w-max text-xs bg-grey  text-white  pb-3 px-1 shadow-xl flex items-center gap-1 rounded-sm"
 			style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 63%, 74% 63%, 59% 100%, 64% 63%, 0 62%)" }}>
+			<MdInfoOutline size={16} />
 			{message}
 		</p>
 	);
