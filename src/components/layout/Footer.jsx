daaf2006b97b22fd80c137dfe16d792053754586
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-22 21:25:13 +0100

    footer style

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { MdCopyright } from "react-icons/md";
 
 function Footer() {
-    return (
-        <footer className='bg-secondary' style={{ zIndex: "100" }}>Footer</footer>
-    )
+	return (
+		<footer className="bg-secondary flex text-white p-5 justify-center " style={{ zIndex: "100" }}>
+			<MdCopyright />
+			2022 Designed and Developed by
+			<a href="https://ankitsoni.netlify.app/" target="_blank" rel="noreferrer" className="ml-1 text-accent font-semibold">
+				AnkSon
+			</a>
+		</footer>
+	);
 }
 
-export default Footer
+export default Footer;
