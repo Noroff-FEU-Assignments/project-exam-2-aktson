b2213a1ae4f05b99271269cfad63faff7a320ed6
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-22 21:12:58 +0100

    animation added tooptip

--- src/components/uiComponents/formTooltip/FormTooltip.jsx
+++ src/components/uiComponents/formTooltip/FormTooltip.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { MdInfoOutline } from "react-icons/md";
+import { motion } from "framer-motion";
 
 function FormTooltip({ message }) {
 	return (
-		<p
+		<motion.p
+			initial={{ scale: 0.9 }}
+			animate={{ scale: 1 }}
+			exit={{ scale: 1 }}
+			transition={{ duration: 0.4 }}
 			className="absolute -top-8 left-0 w-max text-xs bg-grey  text-white  pb-3 px-1 shadow-xl flex items-center gap-1 rounded-sm"
 			style={{ clipPath: "polygon(0% 0%, 100% 0%, 100% 63%, 74% 63%, 59% 100%, 64% 63%, 0 62%)" }}>
 			<MdInfoOutline size={16} />
 			{message}
-		</p>
+		</motion.p>
 	);
 }
 
