29ea8c5fd5dc4da32b36c668c2da0ae7f07177ff
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-22 22:06:14 +0100

    env file chaged

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -1,128 +1,119 @@
-import React from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers/yup';
-import { signInSchema } from '../yupSchema/signinSchema';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInSchema } from "../yupSchema/signinSchema";
 import { Card, CardHeader, CardBody, CardFooter, Typography, Input, Button, Tooltip } from "@material-tailwind/react";
 import { MdVisibility, MdVisibilityOff, MdMailOutline } from "react-icons/md";
-import ErrorSpan from '../uiComponents/ErrorSpan';
-import { LOGIN_URL } from '../constants/api';
-import AuthContext from '../context/AuthContext';
-import { toast } from 'react-toastify';
+import ErrorSpan from "../uiComponents/ErrorSpan";
+import { LOGIN_URL } from "../constants/api";
+import AuthContext from "../context/AuthContext";
+import { toast } from "react-toastify";
 import { motion } from "framer-motion";
-import WrapperSignInUp from '../uiComponents/welcomeBanner/WrapperSignInUp';
-import Spinner from '../uiComponents/loader/Spinner';
-
-
+import WrapperSignInUp from "../uiComponents/welcomeBanner/WrapperSignInUp";
+import Spinner from "../uiComponents/loader/Spinner";
 
 function SignIn() {
+	document.title = "Sign in | ShareIt";
 
-    document.title = "Sign in | ShareIt"
+	const { setAuth } = React.useContext(AuthContext);
 
-    const { setAuth } = React.useContext(AuthContext);
+	const navigate = useNavigate();
 
-    const navigate = useNavigate();
+	// react state
+	const [isVisible, setIsVisible] = React.useState(false);
+	const [isSubmitting, setIsSubmitting] = React.useState(false);
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ resolver: yupResolver(signInSchema) });
 
-    // react state
-    const [isVisible, setIsVisible] = React.useState(false);
-    const [isSubmitting, setIsSubmitting] = React.useState(false);
+	// handles password visibility  icon
+	// on password icon click input type changes to text if visibility is on otherwise opposite
+	const handlePasswordVisibility = () => {
+		setIsVisible((prevState) => !prevState);
+	};
 
-    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(signInSchema) });
+	// handles form submission, validates form and sends to api endpoint sign in request
+	const handleFormSubmit = async (data) => {
+		setIsSubmitting(true);
+		try {
+			const response = await axios.post(LOGIN_URL, data);
 
-    // handles password visibility  icon
-    // on password icon click input type changes to text if visibility is on otherwise opposite 
-    const handlePasswordVisibility = () => {
-        setIsVisible((prevState) => !prevState)
-    }
-    const handleMouseOver = () => {
-        console.log("ankit")
-    }
-
-    // handles form submission, validates form and sends to api endpoint sign in request
-    const handleFormSubmit = async (data) => {
-
-        setIsSubmitting(true)
-        try {
-            const response = await axios.post(LOGIN_URL, data)
-
-            if (response) {
-                setAuth(response.data)
-                navigate("/home")
-            }
-
-        } catch (error) {
-            console.log(error)
-            toast.error(error ? error.response.data.errors[0].message : "Error: Could not log in")
-
-        } finally {
-            setIsSubmitting(false)
-        }
-    }
-    return (
-        <WrapperSignInUp>
-            <motion.form
-                initial={{ scale: 0.9, }}
-                animate={{ scale: 1, }}
-                exit={{ scale: 1, }}
-                transition={{ duration: 0.2 }}
-            >
-                <Card className="w-96 mx-auto">
-                    <CardHeader className="mb-4 grid h-28 place-items-center bg-primary">
-                        <Typography variant="h3" color="white">
-                            Sign In
-                        </Typography>
-                    </CardHeader>
-                    <CardBody className="flex flex-col gap-6"  >
-                        <div>
-                            <Input
-                                {...register("email")}
-                                label="email"
-                                size="lg"
-                                variant="standard"
-                                color="cyan"
-                                onMouseOver={handleMouseOver}
-                                icon={<MdMailOutline size={20} />}
-                            />
-                            {errors.email && <ErrorSpan message={errors.email.message} />}
-                        </div>
-                        <div>
-                            <Input
-                                {...register("password")}
-                                label="Password"
-                                size="lg"
-                                color="cyan"
-                                variant="standard"
-                                type={!isVisible ? "password" : "text"}
-                                icon={!isVisible ?
-                                    <MdVisibilityOff
-                                        size={20} onClick={handlePasswordVisibility} className="cursor-pointer" /> :
-                                    <MdVisibility size={20} onClick={handlePasswordVisibility} className="cursor-pointer" />}
-                            />
-                            {errors.password && <ErrorSpan message={errors.password.message} />}
-                        </div>
-                    </CardBody>
-                    <CardFooter className="pt-0 mt-4">
-                        <Button fullWidth className='flex gap-2 items-center mt-4 justify-center ' color='cyan' onClick={handleSubmit(handleFormSubmit)}>
-                            <Spinner isSubmitting={isSubmitting} />
-                            {isSubmitting ? "Signing In" : "Sign In"}
-                        </Button>
-                        <Typography variant="small" as="div" className="mt-6 flex justify-center">
-                            Don't have an account?
-                            <Typography
-                                as="p"
-                                variant="small"
-                                className="ml-1 font-bold text-primary">
-                                <Link to="/sign-up"> Create Account</Link>
-                            </Typography>
-                        </Typography>
-                    </CardFooter>
-                </Card>
-            </motion.form >
-        </WrapperSignInUp>
-
-    )
+			if (response) {
+				setAuth(response.data);
+				navigate("/home");
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(error ? error?.response.data.errors[0].message : "Error: Could not log in");
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+	return (
+		<WrapperSignInUp>
+			<motion.form initial={{ scale: 0.9 }} animate={{ scale: 1 }} exit={{ scale: 1 }} transition={{ duration: 0.2 }}>
+				<Card className="w-96 mx-auto">
+					<CardHeader className="mb-4 grid h-28 place-items-center bg-primary">
+						<Typography variant="h3" color="white">
+							Sign In
+						</Typography>
+					</CardHeader>
+					<CardBody className="flex flex-col gap-6">
+						<div>
+							<Input
+								{...register("email")}
+								label="email"
+								size="lg"
+								variant="standard"
+								color="cyan"
+								icon={<MdMailOutline size={20} />}
+							/>
+							{errors.email && <ErrorSpan message={errors.email.message} />}
+						</div>
+						<div>
+							<Input
+								{...register("password")}
+								label="Password"
+								size="lg"
+								color="cyan"
+								variant="standard"
+								type={!isVisible ? "password" : "text"}
+								icon={
+									!isVisible ? (
+										<MdVisibilityOff size={20} onClick={handlePasswordVisibility} className="cursor-pointer" />
+									) : (
+										<MdVisibility size={20} onClick={handlePasswordVisibility} className="cursor-pointer" />
+									)
+								}
+							/>
+							{errors.password && <ErrorSpan message={errors.password.message} />}
+						</div>
+					</CardBody>
+					<CardFooter className="pt-0 mt-4">
+						<Button
+							fullWidth
+							className="flex gap-2 items-center mt-4 justify-center "
+							color="cyan"
+							onClick={handleSubmit(handleFormSubmit)}>
+							<Spinner isSubmitting={isSubmitting} />
+							{isSubmitting ? "Signing In" : "Sign In"}
+						</Button>
+						<Typography variant="small" as="div" className="mt-6 flex justify-center">
+							Don't have an account?
+							<Typography as="p" variant="small" className="ml-1 font-bold text-primary">
+								<Link to="/sign-up"> Create Account</Link>
+							</Typography>
+						</Typography>
+					</CardFooter>
+				</Card>
+			</motion.form>
+		</WrapperSignInUp>
+	);
 }
 
-export default SignIn
+export default SignIn;
