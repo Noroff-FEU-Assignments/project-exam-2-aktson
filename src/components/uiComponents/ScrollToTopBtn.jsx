d3428cd52fc5d9b7b6d1773ac176179ebad062ad
Ankit Soni <akt_soni@yahoo.co.in>
2022-11-27 00:15:53 +0100

    animation added

--- src/components/uiComponents/ScrollToTopBtn.jsx
+++ src/components/uiComponents/ScrollToTopBtn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdNorth } from "react-icons/md";
+import { motion } from "framer-motion";
 
 function ScrollToTopBtn() {
 	const [show, setShow] = React.useState(false);
@@ -28,13 +29,17 @@ function ScrollToTopBtn() {
 	return (
 		<>
 			{show && (
-				<button
+				<motion.button
+					initial={{ opacity: 0, translateY: -20 }}
+					animate={{ opacity: 1, translateY: 0 }}
+					exit={{ opacity: 0, translateY: -20 }}
+					transition={{ type: "tween", duration: 0.1, delay: 0.1 }}
 					className="fixed right-2 z-40 bg-primary text-white p-2 rounded-lg shadow-xl hover:scale-90 transition-scale duration-200"
 					color="cyan"
 					onClick={handleScrollClick}
 					style={{ top: "92vh" }}>
 					<MdNorth size={24} />
-				</button>
+				</motion.button>
 			)}
 		</>
 	);
