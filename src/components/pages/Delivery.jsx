c5e82224dcebececafa0a91ec8610a1dbc7e90a5
Ankit Soni <akt_soni@yahoo.co.in>
2022-12-02 01:50:54 +0100

    report added

--- src/components/pages/Delivery.jsx
+++ src/components/pages/Delivery.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Footer from "../layout/Footer";
 import bgImage from "../../assets/bg.jpg";
 import { BRAND } from "../constants/api";
-import pdfFile from "../../assets/gantt-chart_project-exam-2_ankit-soni.pdf";
+import gant from "../../assets/gantt-chart_project-exam-2_ankit-soni.pdf";
+import userTesting from "../../assets/user-testing_report_project-exam-2_ankit-soni.pdf";
+import trelloReport from "../../assets/agile_board_project-exam-2_ankit-soni.png";
 
 function Delivery() {
 	document.title = `Delivery Resaurces | ${BRAND}`;
@@ -29,7 +31,7 @@ function Delivery() {
 						<tr>
 							<td class=" p-2 ">Gantt Chart</td>
 							<td class=" p-2 text-secondary hover:scale-90 transition-all duration-300">
-								<a href={pdfFile} target="_blank" rel="noreferrer">
+								<a href={gant} target="_blank" rel="noreferrer">
 									gantt-chart_project-exam-2_ankit-soni.pdf
 								</a>
 							</td>
@@ -71,6 +73,22 @@ function Delivery() {
 							<td class=" p-2 text-secondary hover:scale-90 transition-all duration-300">
 								<a href="https://socialme.vercel.app/" target="_blank" rel="noreferrer">
 									https://socialme.vercel.app/
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td class=" p-2 ">Trello board report</td>
+							<td class=" p-2 text-secondary hover:scale-90 transition-all duration-300">
+								<a href={trelloReport} target="_blank" rel="noreferrer">
+									agile_board_project-exam-2_ankit-soni
+								</a>
+							</td>
+						</tr>
+						<tr>
+							<td class=" p-2 ">User testing report</td>
+							<td class=" p-2 text-secondary hover:scale-90 transition-all duration-300">
+								<a href={userTesting} target="_blank" rel="noreferrer">
+									user-testing_report_project-exam-2_ankit-soni.pdf
 								</a>
 							</td>
 						</tr>
